f1f293768e8637956fb38785d9694672579f5cf5
KennethMathari <mathari580@gmail.com>
2021-12-19 15:13:14 +0300

    Get tip percentage from radio Button UI

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -11,13 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{ calculateTip() }
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
     fun calculateTip() {
         //Get cost of service
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
+        //Get the tip percentage
+        val selectedId = binding.tipsOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
 
     }
 }
